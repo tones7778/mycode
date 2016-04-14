1fd1d4a961e27a2b81b4c56e886526253bc9cb86
tones7778 <tony.anania@gmail.com>
2016-04-13 21:19:18 -0400

    added one more flask example.

---
+++ flask-listing.py
@@ -0,0 +1,22 @@
+import flask
+import subprocess
+import time          #You don't need this. Just included it so you can see the output stream.
+
+app = flask.Flask(__name__)
+
+@app.route('/hello')
+def index():
+    def inner():
+        proc = subprocess.Popen(
+            ['ls -l'],             #call something with a lot of output so we can see it
+            shell=True,
+            stdout=subprocess.PIPE
+        )
+
+        for line in iter(proc.stdout.readline,''):
+            time.sleep(1)                           # Don't need this just shows the text streaming
+            yield line.rstrip() + '<br/>\n'
+
+    return flask.Response(inner(), mimetype='text/html')  # text/html is required for most browsers to show th$
+
+app.run(debug=True, port=8080, host='0.0.0.0')
